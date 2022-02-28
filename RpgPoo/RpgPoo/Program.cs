73bbfc71c4a407a76cece8bd7765abafc586e9fe
Vinny-PressF <viniciufa@gmail.com>
2022-02-28 19:58:17 -0300

    Primeiros passos do exercicio de POO

---
+++ RpgPoo/RpgPoo/Program.cs
@@ -0,0 +1,6 @@
+﻿using RpgPoo.src.Entities;
+
+Hero[] heroi = new Hero[2];
+heroi[0] = new Hero("Arus", 2, "Knigth");
+heroi[1] = new Hero("sadokaso", 1, "asdas");
+Console.WriteLine(heroi[1].ToString());
---
+++ RpgPoo/RpgPoo/src/Entities/Hero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgPoo.src.Entities;
+
+internal class Hero
+{
+    public String Name { get; private set; }
+    public int Level { get; private set; }
+    public String HeroType { get; private set; }
+
+    public Hero(String name, int level, String type)
+    {
+        this.Name = name;
+        this.Level = level;
+        this.HeroType = type;
+        Console.WriteLine("================\nNome: " + this.Name + "\nLevel: " + this.Level + "\nClasse: " + this.HeroType);
+    }
+}
