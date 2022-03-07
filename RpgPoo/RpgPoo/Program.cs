97befcfdf8f6690ca605d790c6faf59df61f1970
GLaDOS-exe <61170522+GLaDOS-exe@users.noreply.github.com>
2022-03-07 18:29:27 -0300

    Update Program.cs

--- RpgPoo/RpgPoo/Program.cs
+++ RpgPoo/RpgPoo/Program.cs
@@ -1,10 +1,7 @@
-﻿//using RpgPoo.src.Entities;
+﻿using RpgPoo.src.Entities;
 
-//Knigth cavalheiro = new Knigth("Arus", 23);
-//Wizard mago = new Wizard("Raiden", 23);
-//Console.WriteLine(cavalheiro.Attack());
-//Console.WriteLine(mago.Attack());
-
-int valor = 100, valor2 = 0;
-double resultado = valor / valor2;
+Knigth cavalheiro = new Knigth("Arus", 23);
+Wizard mago = new Wizard("Raiden", 23);
+Console.WriteLine(cavalheiro.Attack());
+Console.WriteLine(mago.Attack());
 
